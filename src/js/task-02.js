7452bfca1e3e2b8933c723c3ff8024f1c8a2b3d0
Elena Kvasko <lenakvasko@gmail.com>
2023-01-11 19:28:12 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsRef = document.querySelector('#ingredients');
+const elements = ingredients.map(ingredient => {
+  const itemRef = document.createElement('li');
+  itemRef.classList.add('item');
+  itemRef.textContent = ingredient;
+  return itemRef;
+});
+
+ingredientsRef.append(...elements);
+console.log(elements);
