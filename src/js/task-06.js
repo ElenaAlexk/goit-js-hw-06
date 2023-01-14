a34281b6ad3d311ecf3564c3698e586fc6f9aa28
Elena Kvasko <lenakvasko@gmail.com>
2023-01-14 16:25:36 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -6,15 +6,11 @@ function onInputBlur(event) {
   if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
     inputRef.classList.add('valid');
     inputRef.classList.remove('invalid');
-  }
-
-  if (event.currentTarget.value.length !== Number(inputRef.dataset.length)) {
-    inputRef.classList.add('invalid');
-  }
-
-  if (event.currentTarget.value.length === 0) {
+  } else if (event.currentTarget.value.length === 0) {
     inputRef.classList.remove('valid');
     inputRef.classList.remove('invalid');
+  } else {
+    inputRef.classList.add('invalid');
   }
-  //console.log(event);
 }
+//console.log(event);
