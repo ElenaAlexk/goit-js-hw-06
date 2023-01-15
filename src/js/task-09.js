a358dc7d561b54dfa2d44217c6fb7f013c95b246
Elena Kvasko <lenakvasko@gmail.com>
2023-01-15 20:38:32 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const span = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+button.addEventListener('click', onButtonClick);
+
+function onButtonClick(event) {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+
+  span.textContent = color;
+  console.log(event);
+}
